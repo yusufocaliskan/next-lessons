50bd1fb886f21f21f7cdbdd9f41b136dd1ad2e3d
ooyakari <20151375@std.neu.edu.tr>
2024-03-10 21:24:28 +0100

    Updated post cards and css

--- src/components/PostCard/index.js
+++ src/components/PostCard/index.js
@@ -1,5 +1,6 @@
 import "./postcard.css"
 import "@/app/globals.css"
+import { IoPlayCircleOutline, IoSearchOutline } from "react-icons/io5";
 
 
 const PostCard =({imgSrc,
@@ -11,13 +12,27 @@ const PostCard =({imgSrc,
     
 
     return <>
-    <div className="wrapper">
+    <div className="wrapper container featuredposts">
+        <div className="main-post">
+                <div className="post">
+                    <div className="post-image">
+                    <img src="https://i1.adis.ws/i/canon/future_of_forests_header_16x9_dc14bbe1e35040f79bf566eedaf5c8f7?$hero-header-half-16by9-dt$" />
+                    </div>
+                    <div className="post-informations">
+                    <div className="icon-with-text">
+                        <IoPlayCircleOutline size="22" />
+                        <span>With Video</span>
+                    </div>
+                    <span>Franziskus' Haltung zum Krieg</span>
+                    <h1>Ukraine: Warum der Papst die weiße Fahne will</h1>
+                    </div>
+                </div>
+        </div>
         <div className="card-container">
             <div className="card">
                 <img src={imgSrc} alt={imgAlt} className="card-img" />
                 <h1 className="card-title">{title}</h1>
                 <p className="card-desc">{description}</p>
-                <a href={link} className="card-btn">{buttonText}</a>
             </div>
             
         </div>
@@ -26,28 +41,10 @@ const PostCard =({imgSrc,
                 <img src={imgSrc} alt={imgAlt} className="card-img" />
                 <h1 className="card-title">{title}</h1>
                 <p className="card-desc">{description}</p>
-                <a href={link} className="card-btn">{buttonText}</a>
             </div>
             
         </div>
-        <div className="card-container">
-            <div className="card">
-                <img src={imgSrc} alt={imgAlt} className="card-img" />
-                <h1 className="card-title">{title}</h1>
-                <p className="card-desc">{description}</p>
-                <a href={link} className="card-btn">{buttonText}</a>
-            </div>
-            
-        </div>
-        <div className="card-container">
-            <div className="card">
-                <img src={imgSrc} alt={imgAlt} className="card-img" />
-                <h1 className="card-title">{title}</h1>
-                <p className="card-desc">{description}</p>
-                <a href={link} className="card-btn">{buttonText}</a>
-            </div>
-            
-        </div>
+
        
     </div>
 
